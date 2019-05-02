8b7e6e90cb0e83094c221b5444077f047075358a
Maverick Peppers <bmpeppers@gmail.com>
2019-05-01 23:21:40 -0500

    Input mnager and lib scene documented

--- BattleNetwork/bnInputManager.h
+++ BattleNetwork/bnInputManager.h
@@ -1,39 +1,125 @@
 #pragma once
 #include <vector>
-using std::vector;
 #include <map>
-using std::map;
 
 #include "bnInputEvent.h"
 #include "bnChronoXConfigReader.h"
 
+using std::map;
+using std::vector;
+
+/**
+ * @class InputManager
+ * @author mav
+ * @date 01/05/19
+ * @file bnInputManager.h
+ * @brief Handles SFML event polling, checks for valid controller configurations, and manages event states
+ */
 class InputManager {
 public:
+  /**
+   * @brief Get the singleton instance. If first call, initializes itself.
+   * @return InputManager&
+   */
   static InputManager& GetInstance();
+  
+  /**
+   * @brief Frees config pointer
+   */
   ~InputManager();
+  
+  /**
+   * @brief Polls SFML's event queue used by windows and input devices
+   * 
+   * If controller is enabled, maps controller events to game input events
+   * This is achieved through the configuration object that maps actions to paired
+   * buttons.
+   * 
+   * Before this function ends, it will scan for any release events. If they are found,
+   * they are cleared from the event list. If no release is found but a press is present,
+   * the button state is transformed into a HELD state equivalent.
+   */
   void Update();
+  
+  /**
+   * @brief Queries if an input event has been fired
+   * @param _event the event to look for.
+   * @return true if present, false otherwise
+   */
   bool Has(InputEvent _event);
+  
+  /**
+   * @brief Checks if the input event list is empty
+   * @return true if empty, false otherwise
+   */
   bool Empty();
+  
+  /**
+   * @brief Creates a reference to the config reader object
+   * @param config
+   */
   void SupportChronoXGamepad(ChronoXConfigReader& config);
+  
+  /**
+   * @brief Returns true if the config reader is set, config file is valid, & a joystick is found
+   * @return 
+   */
   bool HasChronoXGamepadSupport();
+  
+  /**
+   * @brief Begins capturing entered text instead of firing game input events
+   * 
+   * Used for name or text entry
+   */
   void BeginCaptureInputBuffer();
+  
+  /**
+   * @brief Ends text capture state
+   * 
+   * Used when leaving input fields
+   */
   void EndCaptureInputBuffer();
+  
+  /**
+   * @brief Returns the contents of the current captured text
+   * @return const std::string
+   */
   const std::string GetInputBuffer();
-  void HandleInputBuffer(sf::Event);
+  
+  /**
+   * @brief Transforms SFML keycodes into ASCII char texts and stores into input buffer
+   * @param e input keycode event
+   */
+  void HandleInputBuffer(sf::Event e);
+  
+  /**
+   * @brief Overloads the input buffer
+   * @param buff text to modify
+   * 
+   * Used when selecting existing input fields
+   * After being set, the buffer can be deleted with backspace or modified by typing
+   */
   void SetInputBuffer(std::string buff);
 
 private:
-  bool captureInputBuffer;
-  std::string inputBuffer;
+  bool captureInputBuffer; /*!< Flags input buffer capture state */
+  std::string inputBuffer; /*!< The internal input buffer data */
 
+  /**
+   * @brief sets all initial input events to false
+   */
   InputManager();
-  vector<InputEvent> events;
-  vector<InputEvent> eventsLastFrame;
+  
+  vector<InputEvent> events; /*!< Current event list */
+  vector<InputEvent> eventsLastFrame; /*!< The even list prior to this update */
 
-  map<std::string, bool> gamepadPressed;
+  map<std::string, bool> gamepadPressed; /*!< Maps controller events*/
 
-  // Support for ChronoX config.ini files
-  ChronoXConfigReader* config;
+  ChronoXConfigReader* config;   /*!< Support for ChronoX config.ini files */
+
 };
 
+/**
+ * @brief macro to shorten manager calls
+ */
 #define INPUT InputManager::GetInstance()
