eaf955527ddd6fb0b1150f5d970cc725aa2a6bd2
Maverick Peppers <bmpeppers@gmail.com>
2019-05-21 11:44:47 -0500

    using new theme. forcing RTD to use doxy file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,11 +48,6 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
@@ -70,7 +65,9 @@ exhale_args = {
         "doxygenStripFromPath": "..",
         "createTreeView":    True,
         "exhaleExecutesDoxygen": True,
-        "exhaleDoxygenStdin": "INPUT = ../../BattleNetwork"
+        "exhaleDoxygenStdin": "INPUT = ../../BattleNetwork",
+        "exhaleUseDoxyfile": True,
+
         }
 
 primary_domain = 'cpp'
@@ -80,8 +77,8 @@ highlight_language = 'cpp'
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if True:
+    import sphinx_bootstrap_theme
+    html_theme = 'bootstrap'
+    html_theme_path = [sphinx_bootstrap_theme.get_html_theme_path()]
 
