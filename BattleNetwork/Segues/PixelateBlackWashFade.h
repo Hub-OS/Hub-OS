1e865225b49d9bf786201e08ecf7c6d365de7cb8
TheMaverickProgrammer <bmpeppers@gmail.com>
2019-05-31 14:39:59 -0500

    resolving one merge at a time....

--- BattleNetwork/Segues/PixelateBlackWashFade.h
+++ BattleNetwork/Segues/PixelateBlackWashFade.h
@@ -5,10 +5,10 @@
 
 using namespace swoosh;
 
+#ifdef __ANDROID__
 namespace {
   auto PIXELATE_SHADER = GLSL
   (
-<<<<<<< HEAD
     100,
     precision lowp float;
     precision lowp int;
@@ -17,131 +17,135 @@ namespace {
     varying vec4 vColor;
 
     uniform sampler2D texture; // Our render texture
-=======
-    110,
-    uniform sampler2D texture;
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
     uniform float pixel_threshold;
 
     void main()
     {
       float factor = 1.0 / (pixel_threshold + 0.001);
-<<<<<<< HEAD
-      vec2 pos = floor(vTexCoord.xy * factor + 0.5) / factor;
-      gl_FragColor = texture2D(texture, pos) * vColor;
-=======
       vec2 pos = floor(gl_TexCoord[0].xy * factor + 0.5) / factor;
       gl_FragColor = texture2D(texture, pos) * gl_Color;
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
     }
   );
 }
+#else
+namespace {
+  auto PIXELATE_SHADER = GLSL
+  (
+    100,
+    precision lowp float;
+    precision lowp int;
+
+    varying vec2 vTexCoord;
+    varying vec4 vColor;
+
+    uniform sampler2D texture; // Our render texture
+    uniform float pixel_threshold;
+
+    void main()
+    {
+      float factor = 1.0 / (pixel_threshold + 0.001);
+      vec2 pos = floor(vTexCoord.xy * factor + 0.5) / factor;
+      gl_FragColor = texture2D(texture, pos) * vColor;
+    }
+  );
+}
+#endif
 
 class PixelateBlackWashFade : public Segue {
 private:
-  sf::Shader shader;
-  float factor;
-<<<<<<< HEAD
-  bool loaded;
-  bool nextScreen;
+    sf::Shader shader;
+    float factor;
+    bool loaded;
+    bool nextScreen;
 
-  sf::Texture* temp;
-=======
+    sf::Texture *temp;
 
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
 public:
-  virtual void onDraw(sf::RenderTexture& surface) {
-    double elapsed = getElapsed().asMilliseconds();
-    double duration = getDuration().asMilliseconds();
-    double alpha = ease::wideParabola(elapsed, duration, 1.0);
+    virtual void onDraw(sf::RenderTexture &surface) {
+        double elapsed = getElapsed().asMilliseconds();
+        double duration = getDuration().asMilliseconds();
+        double alpha = ease::wideParabola(elapsed, duration, 1.0);
 
-    if (elapsed <= duration * 0.5)
-      this->drawLastActivity(surface);
-<<<<<<< HEAD
-    else {
-        if(loaded && !nextScreen) {
-            loaded = false;
-            nextScreen = true;
+        if (elapsed <= duration * 0.5)
+            this->drawLastActivity(surface);
+        else {
+            if (loaded && !nextScreen) {
+                loaded = false;
+                nextScreen = true;
+            }
+
+            this->drawNextActivity(surface);
         }
 
-        this->drawNextActivity(surface);
-    }
-
-    surface.display();
+        surface.display();
 
 #ifdef __ANDROID__
-    if(!loaded) {
-        temp = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
-        temp->flip(true);
-        loaded = true;
-    }
+        if (!loaded) {
+            temp = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
+            temp->flip(true);
+            loaded = true;
+        }
 #else
-      temp = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
+        temp = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
 #endif
 
-=======
-    else
-      this->drawNextActivity(surface);
+        sf::Sprite sprite(*temp);
 
-    surface.display();
-    sf::Texture* temp = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
-    sf::Sprite sprite(*temp);
+        shader.setUniform("texture", *temp);
+        shader.setUniform("pixel_threshold", (float) alpha / 15.0f);
+        sf::RenderStates states;
+        states.shader = &shader;
 
-    shader.setUniform("texture", *temp);
-    shader.setUniform("pixel_threshold", (float)alpha/15.0f);
-    sf::RenderStates states;
-    states.shader = &shader;
+        surface.clear(sf::Color::Transparent);
+        surface.draw(sprite, states);
 
-    surface.clear(sf::Color::Transparent);
-    surface.draw(sprite, states);
+        // 10% of segue is a pixelate before darkening
+        double delay = (duration / 10.0);
+        if (elapsed > delay) {
+            double alpha = ease::wideParabola(elapsed - delay, duration - delay, 1.0);
 
-    // 10% of segue is a pixelate before darkening
-    double delay = (duration / 10.0);
-    if (elapsed > delay) {
-      double alpha = ease::wideParabola(elapsed - delay, duration - delay, 1.0);
-
-      sf::RectangleShape whiteout;
-      whiteout.setSize(sf::Vector2f((float)surface.getTexture().getSize().x, (float)surface.getTexture().getSize().y));
-      whiteout.setFillColor(sf::Color(0, 0, 0, (sf::Uint8)(alpha * (double)255)));
-      surface.draw(whiteout);
+            sf::RectangleShape whiteout;
+            whiteout.setSize(sf::Vector2f((float) surface.getTexture().getSize().x,
+                                          (float) surface.getTexture().getSize().y));
+            whiteout.setFillColor(sf::Color(0, 0, 0, (sf::Uint8) (alpha * (double) 255)));
+            surface.draw(whiteout);
+        }
     }
-  }
 
-  PixelateBlackWashFade(sf::Time duration, Activity* last, Activity* next) : Segue(duration, last, next) {
-<<<<<<< HEAD
-      loaded = nextScreen = false;
+    PixelateBlackWashFade(sf::Time duration, Activity *last, Activity *next) : Segue(duration, last,
+                                                                                     next) {
+        loaded = nextScreen = false;
 
-    shader.loadFromMemory("uniform mat4 viewMatrix;\n"
-                         "uniform mat4 projMatrix;\n"
-                         "uniform mat4 textMatrix;\n"
-                         " \n"
-                         "attribute vec2 position;\n"
-                         "attribute vec4 color;\n"
-                         "attribute vec2 texCoord;\n"
-                         "\n"
-                         "varying vec4 vColor;\n"
-                         "varying vec2 vTexCoord;\n"
-                         " \n"
-                         "void main()\n"
-                         "{\n"
-                         "    gl_Position = projMatrix * viewMatrix * vec4(position, 0.0, 1.0);\n"
-                         "    vColor = color;\n"
-                         "    vTexCoord = (textMatrix * vec4(texCoord.xy, 0.0, 1.0)).xy;\n"
-                         "}",::PIXELATE_SHADER);
-  }
-
-  virtual ~PixelateBlackWashFade() {
 #ifdef __ANDROID__
-      if(temp) {
-          delete temp;
-      }
-#endif
-  }
-=======
-    shader.loadFromMemory(::PIXELATE_SHADER, sf::Shader::Fragment);
-  }
+        shader.loadFromMemory("uniform mat4 viewMatrix;\n"
+                              "uniform mat4 projMatrix;\n"
+                              "uniform mat4 textMatrix;\n"
+                              " \n"
+                              "attribute vec2 position;\n"
+                              "attribute vec4 color;\n"
+                              "attribute vec2 texCoord;\n"
+                              "\n"
+                              "varying vec4 vColor;\n"
+                              "varying vec2 vTexCoord;\n"
+                              " \n"
+                              "void main()\n"
+                              "{\n"
+                              "    gl_Position = projMatrix * viewMatrix * vec4(position, 0.0, 1.0);\n"
+                              "    vColor = color;\n"
+                              "    vTexCoord = (textMatrix * vec4(texCoord.xy, 0.0, 1.0)).xy;\n"
+                              "}", ::PIXELATE_SHADER);
 
-  virtual ~PixelateBlackWashFade() { ; }
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
+#else
+        shader.loadFromMemory(::PIXELATE_SHADER, sf::Shader::Fragment);
+#endif
+    }
+
+    virtual ~PixelateBlackWashFade() {
+#ifdef __ANDROID__
+        if (temp) {
+            delete temp;
+        }
+#endif
+    }
 };
+
--- BattleNetwork/Segues/PushIn.h
+++ BattleNetwork/Segues/PushIn.h
@@ -7,7 +7,6 @@ using namespace swoosh;
 template<int direction>
 class PushIn : public Segue {
 private:
-<<<<<<< HEAD
     sf::Texture temp, temp2;
     bool loaded;
 
@@ -75,60 +74,4 @@ public:
     }
 
     virtual ~PushIn() { }
-=======
-  sf::Texture* temp;
-
-public:
-
-  virtual void onDraw(sf::RenderTexture& surface) {
-    double elapsed = getElapsed().asMilliseconds();
-    double duration = getDuration().asMilliseconds();
-    double alpha = ease::linear(elapsed, duration, 1.0);
-
-    this->drawLastActivity(surface);
-
-    surface.display(); // flip and ready the buffer
-
-    if (temp) delete temp;
-    temp = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
-
-    sf::Sprite left(*temp);
-
-    int lr = 0;
-    int ud = 0;
-
-    if (direction == 0) lr = -1;
-    if (direction == 1) lr = 1;
-    if (direction == 2) ud = -1;
-    if (direction == 3) ud = 1;
-
-    left.setPosition((float)(lr * alpha * left.getTexture()->getSize().x), (float)(ud * alpha * left.getTexture()->getSize().y));
-
-    surface.clear(sf::Color::Transparent);
-
-    this->drawNextActivity(surface);
-
-    surface.display(); // flip and ready the buffer
-
-    auto temp2 = new sf::Texture(surface.getTexture());
-    sf::Sprite right(*temp2);
-
-    right.setPosition((float)(-lr * (1.0-alpha) * right.getTexture()->getSize().x), (float)(-ud * (1.0-alpha) * right.getTexture()->getSize().y));
-
-    sf::RenderWindow& window = getController().getWindow();
-    window.draw(left);
-    window.draw(right);
-
-    surface.clear(sf::Color::Transparent);
-
-    delete temp2;
-  }
-
-  PushIn(sf::Time duration, Activity* last, Activity* next) : Segue(duration, last, next) {
-    /* ... */
-    temp = nullptr;
-  }
-
-  virtual ~PushIn() { if(temp) delete temp; }
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
 };
