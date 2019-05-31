5fb634c1eb13f66f365e48e7dd2e092d6a9c432d
TheMaverickProgrammer <bmpeppers@gmail.com>
2019-05-31 14:01:00 -0500

    resolving one merge at a time....

--- BattleNetwork/Segues/Checkerboard.h
+++ BattleNetwork/Segues/Checkerboard.h
@@ -5,10 +5,10 @@
 
 using namespace swoosh;
 
+#ifdef __ANDROID__
 namespace {
   auto CHECKERBOARD_FRAG_SHADER = GLSL
   (
-<<<<<<< HEAD
     100,
     precision highp float;
     precision highp int;
@@ -17,9 +17,6 @@ namespace {
     varying vec2 vTexCoord;
     varying vec4 vColor;
 
-=======
-    110,
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
     uniform sampler2D texture;
     uniform sampler2D texture2;
     uniform float progress;
@@ -32,11 +29,8 @@ namespace {
     }
 
     void main() {
-<<<<<<< HEAD
       vec2 p = vTexCoord.xy;
-=======
-      vec2 p = gl_TexCoord[0].xy;
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
+
       vec2 size = vec2(cols, rows);
       float r = rand(floor(vec2(size) * p));
       float m = smoothstep(0.0, -smoothness, r - (progress * (1.0 + smoothness)));
@@ -45,18 +39,40 @@ namespace {
 
    );
 }
+#else
+namespace {
+  auto CHECKERBOARD_FRAG_SHADER = GLSL
+  (
+    110,
+    uniform sampler2D texture;
+    uniform sampler2D texture2;
+    uniform float progress;
+    uniform float smoothness; // = 0.5
+    uniform int cols;
+    uniform int rows;
+
+    float rand(vec2 co) {
+      return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
+    }
+
+    void main() {
+      vec2 p = gl_TexCoord[0].xy;
+      vec2 size = vec2(cols, rows);
+      float r = rand(floor(vec2(size) * p));
+      float m = smoothstep(0.0, -smoothness, r - (progress * (1.0 + smoothness)));
+      gl_FragColor = mix(texture2D(texture, p.xy), texture2D(texture2, p.xy), m);
+    }
+
+   );
+}
+#endif
 
 template<int cols, int rows>
 class CheckerboardCustom : public Segue {
 private:
-<<<<<<< HEAD
   sf::Texture* temp, * temp2;
   sf::Shader shader;
   bool loaded;
-=======
-  sf::Texture* temp;
-  sf::Shader shader;
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
 
 public:
   virtual void onDraw(sf::RenderTexture& surface) {
@@ -68,7 +84,6 @@ public:
 
     surface.display(); // flip and ready the buffer
 
-<<<<<<< HEAD
 #ifdef __ANDROID__
     if (!loaded) {
         temp = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
@@ -78,10 +93,6 @@ public:
     if(temp) delete temp;
     temp = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
 #endif
-=======
-    if (temp) delete temp;
-    temp = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
 
     sf::Sprite sprite(*temp);
 
@@ -90,7 +101,6 @@ public:
 
     surface.display(); // flip and ready the buffer
 
-<<<<<<< HEAD
 #ifdef __ANDROID__
 if(!loaded) {
     temp2 = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
@@ -101,9 +111,6 @@ if(!loaded) {
     if(temp2) delete temp2;
     temp2 = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
 #endif
-=======
-    sf::Texture* temp2 = new sf::Texture(surface.getTexture()); // Make a copy of the source texture
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
 
     shader.setUniform("progress", (float)alpha);
     shader.setUniform("texture2", *temp2);
@@ -113,19 +120,16 @@ if(!loaded) {
     states.shader = &shader;
 
     surface.draw(sprite, states);
-<<<<<<< HEAD
-=======
 
     delete temp2;
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
   }
 
   CheckerboardCustom(sf::Time duration, Activity* last, Activity* next) : Segue(duration, last, next) {
     /* ... */
     temp = nullptr;
-<<<<<<< HEAD
     loaded = false;
 
+#ifdef __ANDROID__
     shader.loadFromMemory("uniform mat4 viewMatrix;\n"
                           "uniform mat4 projMatrix;\n"
                           "uniform mat4 textMatrix;\n"
@@ -143,23 +147,20 @@ if(!loaded) {
                           "    vColor = color;\n"
                           "    vTexCoord = (textMatrix * vec4(texCoord.xy, 0.0, 1.0)).xy;\n"
                           "}", ::CHECKERBOARD_FRAG_SHADER );
-=======
 
+#else
     shader.loadFromMemory(::CHECKERBOARD_FRAG_SHADER, sf::Shader::Fragment);
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
+#endif
+
     shader.setUniform("cols", cols);
     shader.setUniform("rows", rows);
     shader.setUniform("smoothness", 0.09f);
   }
 
-<<<<<<< HEAD
   virtual ~CheckerboardCustom() {
       if(temp) delete temp;
       if(temp2) delete temp2;
   }
-=======
-  virtual ~CheckerboardCustom() { ; }
->>>>>>> b486e21e11627262088deae73097eaa7af56791c
 };
 
 using Checkerboard = CheckerboardCustom<10, 10>;
