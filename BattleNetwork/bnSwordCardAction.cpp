5915a6ec01fd13cf70db9fbded78aeb3456102bd
Mav <bmpeppers@gmail.com>
2020-10-28 23:42:33 -0500

    sword fixed. TODO: finish TFC cards and logic

--- BattleNetwork/bnSwordCardAction.cpp
+++ BattleNetwork/bnSwordCardAction.cpp
@@ -37,8 +37,8 @@ SwordCardAction::SwordCardAction(Character * owner, int damage) :
   hiltAnim.Reload();
   hiltAnim.SetAnimation("HILT");
 
-  AddAttachment(userAnim->GetAnimationObj(), "HILT", *hilt).UseAnimation(hiltAnim);
-  AddAttachment(hiltAnim, "ENDPOINT", *blade).UseAnimation(bladeAnim);
+  auto& hiltAttachment = AddAttachment(userAnim->GetAnimationObj(), "HILT", *hilt).UseAnimation(hiltAnim);
+  hiltAttachment.AddAttachment(hiltAnim, "ENDPOINT", *blade).UseAnimation(bladeAnim);
 
   element = Element::none;
 
