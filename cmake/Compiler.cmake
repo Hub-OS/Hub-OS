409ac228096342677f4d65700bd74259b32f03b0
Mav <bmpeppers@gmail.com>
2020-12-06 05:26:40 -0600

    updated cmake file

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -2,10 +2,9 @@
 # This file is included after project and after target declarations in parent CMakeLists.
 
 if(MSVC)
-
     set_property(TARGET BattleNetwork 
                 PROPERTY VS_DEBUGGER_WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/build/$<CONFIG>")
-
+	
 endif()
 
 if(NOT MSVC)
