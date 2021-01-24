1afce20563d81b835b57c21bd6f9904b41200212
Arthur <arthurcose@gmail.com>
2021-01-24 07:54:29 -0500

    Allow fonts to be passed to TextBox

--- BattleNetwork/bnTextBox.cpp
+++ BattleNetwork/bnTextBox.cpp
@@ -2,8 +2,11 @@
 #include "bnAudioResourceManager.h"
 #include "bnTextureResourceManager.h"
 
-TextBox::TextBox(int width, int height) : 
-  font(Font::Style::thin), 
+TextBox::TextBox(int width, int height) :
+  TextBox::TextBox(width, height, Font::Style::thin) { } 
+
+TextBox::TextBox(int width, int height, const Font& font) : 
+  font(font), 
   text("", font) {
   text.scale(2.0f, 2.0f);
   message = "";
