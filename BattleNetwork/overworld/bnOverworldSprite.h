43707fdeaee26fde5f0a8818824155eecde798c3
Arthur <arthurcose@gmail.com>
2021-02-23 19:39:42 -0500

    Remove redundant GetLayer/SetLayer methods

--- BattleNetwork/overworld/bnOverworldSprite.h
+++ BattleNetwork/overworld/bnOverworldSprite.h
@@ -7,15 +7,11 @@
 namespace Overworld {
   class WorldSprite : public SpriteProxyNode {
     public:
-      int GetLayer() const { return layer; }
-      void SetLayer(int layer) { this->layer = layer; };
-
       sf::Transform GetPreTransform() const { return preTransform; }
       void SetPreTransform(sf::Transform transform) { preTransform = transform; }
 
       virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
     private:
       sf::Transform preTransform;
-      int layer{ 0 };
   };
 }
