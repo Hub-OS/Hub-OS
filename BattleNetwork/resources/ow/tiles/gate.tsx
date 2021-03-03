048452a42f3a7fef372afa83bae23b4f88026180
Arthur <arthurcose@gmail.com>
2021-03-03 17:33:49 -0500

    Update gate width

--- BattleNetwork/resources/ow/tiles/gate.tsx
+++ BattleNetwork/resources/ow/tiles/gate.tsx
@@ -1,14 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.1" name="gate" tilewidth="34" tileheight="52" tilecount="5" columns="5" objectalignment="bottom">
+ <grid orientation="isometric" width="64" height="32"/>
  <properties>
   <property name="Solid" type="bool" value="true"/>
  </properties>
  <image source="gate.png" width="170" height="52"/>
  <tile id="0">
   <objectgroup draworder="index" id="3">
-   <object id="2" x="-8.5" y="19.5">
-    <polygon points="25.5,15.5 -6.5,31.5 25.5,47.5 57.5,31.5"/>
-   </object>
+   <object id="3" x="7" y="22.5" width="33" height="32"/>
   </objectgroup>
   <animation>
    <frame tileid="0" duration="50"/>
