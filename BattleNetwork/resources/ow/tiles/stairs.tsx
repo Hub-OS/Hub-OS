2ceffe0b737d0d2b8d67f497ef5fe9f85f2144a8
Arthur <arthurcose@gmail.com>
2021-03-20 17:23:43 -0400

    Add stairs asset

---
+++ BattleNetwork/resources/ow/tiles/stairs.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="stairs" tilewidth="64" tileheight="56" tilecount="3" columns="3">
+ <tileoffset x="-3" y="9"/>
+ <image source="stairs.png" width="192" height="56"/>
+</tileset>
