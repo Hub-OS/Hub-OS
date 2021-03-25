7c5b2648d1b88ecbe19bf5658a7889d0888bbe5a
Arthur <arthurcose@gmail.com>
2021-03-24 22:58:28 -0400

    Update packet version

--- BattleNetwork/overworld/bnPacketHeaders.h
+++ BattleNetwork/overworld/bnPacketHeaders.h
@@ -5,8 +5,8 @@
 
 namespace Overworld
 {
-  const std::string VERSION_ID = "https://github.com/ArthurCose/Scriptable-OpenNetBattle-Server/tree/development";
-  const uint64_t VERSION_ITERATION = 4;
+  const std::string VERSION_ID = "https://github.com/ArthurCose/Scriptable-OpenNetBattle-Server";
+  const uint64_t VERSION_ITERATION = 3;
 
   constexpr double PACKET_RESEND_RATE = 1.0 / 20.0;
 
