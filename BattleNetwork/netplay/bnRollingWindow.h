8eabcd1f3558557d73b4a3c3b266e8a589cf16b6
Arthur Cosentino <arthurcose@gmail.com>
2021-07-10 20:14:28 -0400

    Adjust RollingWindow

--- BattleNetwork/netplay/bnRollingWindow.h
+++ BattleNetwork/netplay/bnRollingWindow.h
@@ -1,10 +1,8 @@
 #pragma once
 
-template<typename T>
+template<typename T, size_t WINDOW_LEN>
 class RollingWindow {
 public:
-  static constexpr size_t WINDOW_LEN = 1000ll;
-
   size_t Length() {
     return currLen;
   }
@@ -13,16 +11,25 @@ public:
     return ema;
   }
 
+  void SetSmoothing(double value) {
+    smoothing = value;
+  }
+
   void Push(T entry) {
     currLen = std::min(currLen + 1, WINDOW_LEN);
 
-    double smoothing = 2.0;
+    if (currLen == 1) {
+      ema = entry;
+      return;
+    }
+
     double newEma = (static_cast<double>(entry) * (smoothing / (1.0 + WINDOW_LEN)))
       + static_cast<double>(ema) * (1.0 - (smoothing / (1.0 + WINDOW_LEN)));
 
     ema = static_cast<T>(newEma);
   }
 private:
+  double smoothing{ 2 };
   T ema{};
   size_t currLen{};
 };
