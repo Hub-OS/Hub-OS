1b00ab8c959f016b2f3c7166dbeaa5ea6da56fe7
Arthur Cosentino <arthurcose@gmail.com>
2022-11-11 02:48:09 -0500

    Remove broken Component constructor

--- client/src/lua_api/battle_api/component_api.rs
+++ client/src/lua_api/battle_api/component_api.rs
@@ -2,37 +2,9 @@ use super::errors::{component_not_found, entity_not_found};
 use super::{BattleLuaApi, COMPONENT_TABLE, UPDATE_FN};
 use crate::battle::*;
 use crate::bindable::*;
-use crate::lua_api::helpers::inherit_metatable;
 
 pub fn inject_component_api(lua_api: &mut BattleLuaApi) {
     // constructor in entity_api.rs Entity:create_component
-
-    lua_api.add_dynamic_function(COMPONENT_TABLE, "new", |api_ctx, lua, params| {
-        let (entity_table, lifetime): (rollback_mlua::Table, ComponentLifetime) =
-            lua.unpack_multi(params)?;
-
-        let entity_id: EntityID = entity_table.get("#id")?;
-
-        let api_ctx = &mut *api_ctx.borrow_mut();
-        let entities = &mut api_ctx.simulation.entities;
-
-        let entity = entities
-            .query_one_mut::<&mut Entity>(entity_id.into())
-            .map_err(|_| entity_not_found())?;
-
-        let component = Component::new(entity_id, lifetime);
-        let id = api_ctx.simulation.components.insert(component);
-
-        if lifetime == ComponentLifetime::Local {
-            entity.local_components.push(id);
-        }
-
-        let table = lua.create_table()?;
-        table.raw_set("#id", GenerationalIndex::from(id))?;
-        inherit_metatable(lua, COMPONENT_TABLE, &table)?;
-
-        lua.pack_multi(table)
-    });
 
     lua_api.add_dynamic_setter(COMPONENT_TABLE, UPDATE_FN, |api_ctx, lua, params| {
         let (table, callback): (rollback_mlua::Table, rollback_mlua::Function) =
