7b0b88fc00dff1a5523b66cd5533db7b333ad00f
Arthur Cosentino <arthurcose@gmail.com>
2023-02-19 19:40:11 -0500

    Fix cancelled warp effects keeping exclusion lock

--- client/src/overworld/components/warp_effect.rs
+++ client/src/overworld/components/warp_effect.rs
@@ -43,14 +43,11 @@ impl WarpEffect {
         callback: Box<dyn FnOnce(&mut GameIO, &mut OverworldSceneBase) + Send + Sync>,
         warp_type: WarpType,
     ) -> hecs::Entity {
-        let position = *base_scene
-            .entities
-            .query_one_mut::<&Vec3>(target_entity)
-            .unwrap();
+        let entities = &mut base_scene.entities;
+        let position = *entities.query_one_mut::<&Vec3>(target_entity).unwrap();
+        let screen_position = base_scene.map.world_3d_to_screen(position);
 
         let globals = game_io.resource::<Globals>().unwrap();
-
-        let screen_position = base_scene.map.world_3d_to_screen(position);
 
         if base_scene.ui_camera.bounds().contains(screen_position) {
             globals.audio.play_sound(&globals.appear_sfx);
@@ -63,7 +60,7 @@ impl WarpEffect {
         match warp_type {
             WarpType::In { .. } => {
                 animator.set_state("IN");
-                Excluded::increment(&mut base_scene.entities, target_entity);
+                Excluded::increment(entities, target_entity);
             }
             WarpType::Out | WarpType::Full { .. } => {
                 animator.set_state("OUT");
@@ -72,8 +69,8 @@ impl WarpEffect {
 
         let warp_entity = base_scene.spawn_artifact(game_io, texture, animator, position);
 
-        base_scene
-            .entities
+        let entities = &mut base_scene.entities;
+        entities
             .insert_one(
                 warp_entity,
                 WarpEffect {
@@ -85,13 +82,19 @@ impl WarpEffect {
             )
             .unwrap();
 
-        let warp_controller = base_scene
-            .entities
+        let warp_controller = entities
             .query_one_mut::<&mut WarpController>(target_entity)
             .unwrap();
 
+        let already_warped_out = warp_controller.warped_out;
+
         warp_controller.warp_entity = Some(warp_entity);
         warp_controller.warped_out = true;
+
+        if already_warped_out {
+            // already warped out
+            Excluded::decrement(entities, target_entity);
+        }
 
         warp_entity
     }
