7b4729a0e462c8b25df6cc8cdb6230153d76bc02
Arthur Cosentino <arthurcose@gmail.com>
2023-04-22 11:19:07 -0400

    Significantly reduce garbage with unsafe trade off

--- client/src/lua_api/battle_api/mod.rs
+++ client/src/lua_api/battle_api/mod.rs
@@ -106,6 +106,4 @@ const INIT_FN: &str = "on_init_func";
 // naming conflicts with table names are avoided by using snake_case
 pub const VM_INDEX_REGISTRY_KEY: &str = "vm_index";
 pub const NAMESPACE_REGISTRY_KEY: &str = "namespace";
-const DELEGATE_TYPE_REGISTRY_KEY: &str = "delegate";
-const DELEGATE_REGISTRY_KEY: &str = "delegate_type";
 const TILE_CACHE_REGISTRY_KEY: &str = "tiles";
