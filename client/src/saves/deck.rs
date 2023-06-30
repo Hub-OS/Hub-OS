b98b453558b35d04c6405919c8cfcf12b094bfd2
Harpy <seventh_sage@hotmail.com>
2023-06-30 12:16:46 -0400

    commenting my code

--- client/src/saves/deck.rs
+++ client/src/saves/deck.rs
@@ -24,18 +24,24 @@ impl Deck {
         let globals = game_io.resource::<Globals>().unwrap();
         let mut is_shuffled = false;
         self.cards.shuffle(rng);
+        // If the deck is larger than 10 cards
         if self.cards.len() >= 10 {
+            // Cycle the initial 10 starting at index 0
             for count in 0..9 {
+                // If we already shuffled, make sure to reset it
                 if is_shuffled == true {
                     is_shuffled = false;
                 }
+                // Get the card we're on, skip loop if it's blank somehow
                 let card_item = &self.cards[count];
                 let Some(package) = globals.card_packages.package_or_fallback(PackageNamespace::Local, &card_item.package_id) else {
                     continue;
                 };
+                // If it's a giga class, loop until we can shuffle it somewhere safe by cycling it forward until we find a safe slot
+                // that's greater than index 9 (aka, 10th card in the deck)
                 if package.card_properties.card_class == CardClass::Giga {
                     while is_shuffled == false {
-                        let rand_slot = rng.gen_range(10..self.cards.len());
+                        let rand_slot = rng.gen_range(9..self.cards.len());
                         let rand_card = &self.cards[rand_slot];
                         let Some(rand_package) = globals.card_packages.package_or_fallback(PackageNamespace::Local, &rand_card.package_id) else {
                             continue;
