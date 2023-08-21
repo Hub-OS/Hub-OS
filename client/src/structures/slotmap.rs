2e1d2699668204ef3f1d59873ab8727a0e665e69
Arthur Cosentino <arthurcose@gmail.com>
2023-08-21 10:03:23 -0400

    Fix ambiguity for github builds

--- client/src/structures/slotmap.rs
+++ client/src/structures/slotmap.rs
@@ -1,10 +1,10 @@
 use super::GenerationalIndex;
 
 // best for random access
-pub type SlotMap<V> = slotmap::SlotMap<GenerationalIndex, V>;
+pub type SlotMap<V> = ::slotmap::SlotMap<GenerationalIndex, V>;
 
 // good for iteration, good for random access
-pub type HopSlotMap<V> = slotmap::HopSlotMap<GenerationalIndex, V>;
+pub type HopSlotMap<V> = ::slotmap::HopSlotMap<GenerationalIndex, V>;
 
 // best for iteration, bad random access
-pub type DenseSlotMap<V> = slotmap::DenseSlotMap<GenerationalIndex, V>;
+pub type DenseSlotMap<V> = ::slotmap::DenseSlotMap<GenerationalIndex, V>;
