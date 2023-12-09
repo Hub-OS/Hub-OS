1049c210c09b79707a68c259997154b90a89d97d
Arthur Cosentino <arthurcose@gmail.com>
2023-12-09 07:06:30 -0500

    Move Net.set_object_type() to deprecated_functions.lua

--- server/src/plugins/lua/api/object_api.rs
+++ server/src/plugins/lua/api/object_api.rs
@@ -151,25 +151,6 @@ pub fn inject_dynamic(lua_api: &mut LuaApi) {
         }
     });
 
-    lua_api.add_dynamic_function("Net", "set_object_type", |api_ctx, lua, params| {
-        log::warn!("Net.set_object_type() is deprecated, use Net.set_object_class()");
-
-        let (area_id, id, class): (mlua::String, u32, String) = lua.unpack_multi(params)?;
-        let area_id_str = area_id.to_str()?;
-
-        let mut net = api_ctx.net_ref.borrow_mut();
-
-        if let Some(area) = net.get_area_mut(area_id_str) {
-            let map = area.map_mut();
-
-            map.set_object_class(id, class);
-
-            lua.pack_multi(())
-        } else {
-            Err(create_area_error(area_id_str))
-        }
-    });
-
     lua_api.add_dynamic_function(
         "Net",
         "set_object_custom_property",
