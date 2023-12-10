dbc89039e06dc4be1049a0b4f2a9a1f56cb0be37
Arthur Cosentino <arthurcose@gmail.com>
2023-12-10 07:38:18 -0500

    Fix address parsing to allow for unspecified port

--- packets/src/address_parsing.rs
+++ packets/src/address_parsing.rs
@@ -8,17 +8,18 @@ pub async fn resolve_socket_addr(mut address: &str) -> Option<SocketAddr> {
     address = strip_data(address);
 
     let port_index = address.find(':');
+    let host = &address[..port_index.unwrap_or(address.len())];
 
-    let socket_addr = if address == "localhost" || address.starts_with("localhost:") {
-        let port = match port_index {
-            Some(index) => address[index + 1..].parse().ok()?,
-            None => DEFAULT_PORT,
-        };
+    let port: u16 = match port_index {
+        Some(index) => address[index + 1..].parse().ok()?,
+        None => DEFAULT_PORT,
+    };
 
+    let socket_addr = if host == "localhost" {
         SocketAddr::new(Ipv4Addr::LOCALHOST.into(), port)
     } else {
         use async_std::net::ToSocketAddrs;
-        let mut socket_addrs = address.to_socket_addrs().await.ok()?;
+        let mut socket_addrs = (host, port).to_socket_addrs().await.ok()?;
 
         let mut socket_addr = socket_addrs.next()?;
 
