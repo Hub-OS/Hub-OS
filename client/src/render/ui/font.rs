a49185e65e70213a7babdc806852e2d8950dfb2b
Arthur Cosentino <arthurcose@gmail.com>
2024-01-01 17:28:49 -0500

    Remove unused Font::state_prefix function

--- client/src/render/ui/font.rs
+++ client/src/render/ui/font.rs
@@ -22,24 +22,6 @@ pub enum FontStyle {
 }
 
 impl FontStyle {
-    pub fn state_prefix(&self) -> &'static str {
-        match self {
-            FontStyle::Thick => "THICK_U",
-            FontStyle::Small => "SMALL_U",
-            FontStyle::Tiny => "TINY_U",
-            FontStyle::Context => "CONTEXT_U",
-            FontStyle::Wide => "WIDE_U",
-            FontStyle::Thin => "THIN_U",
-            FontStyle::Gradient => "GRADIENT_U",
-            FontStyle::GradientGold => "GRADIENT_GOLD_U",
-            FontStyle::GradientGreen => "GRADIENT_GREEN_U",
-            FontStyle::GradientOrange => "GRADIENT_ORANGE_U",
-            FontStyle::GradientTall => "GRADIENT_TALL_U",
-            FontStyle::Battle => "BATTLE_U",
-            FontStyle::EntityHP => "ENTITY_HP_U",
-        }
-    }
-
     pub fn from_state_prefix(state_prefix: &str) -> Option<Self> {
         let font_style = match state_prefix {
             "THICK_U+" => FontStyle::Thick,
