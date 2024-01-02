df5ad647e23df03616e72f8b821112fd7442c08f
Arthur Cosentino <arthurcose@gmail.com>
2024-01-02 15:40:19 -0500

    Translate AnimatorPlaybackMode to Lua integer

--- client/src/bindable/animator_playback_mode.rs
+++ client/src/bindable/animator_playback_mode.rs
@@ -17,7 +17,8 @@ impl<'lua> rollback_mlua::FromLua<'lua> for AnimatorPlaybackMode {
         use num_traits::FromPrimitive;
 
         let number = match lua_value {
-            rollback_mlua::Value::Number(number) => number,
+            rollback_mlua::Value::Number(number) => number as u8,
+            rollback_mlua::Value::Integer(number) => number as u8,
             _ => {
                 return Err(rollback_mlua::Error::FromLuaConversionError {
                     from: lua_value.type_name(),
@@ -27,13 +28,11 @@ impl<'lua> rollback_mlua::FromLua<'lua> for AnimatorPlaybackMode {
             }
         };
 
-        AnimatorPlaybackMode::from_u8(number as u8).ok_or(
-            rollback_mlua::Error::FromLuaConversionError {
-                from: lua_value.type_name(),
-                to: "AnimatorPlaybackMode",
-                message: None,
-            },
-        )
+        AnimatorPlaybackMode::from_u8(number).ok_or(rollback_mlua::Error::FromLuaConversionError {
+            from: lua_value.type_name(),
+            to: "AnimatorPlaybackMode",
+            message: None,
+        })
     }
 }
 
@@ -42,6 +41,6 @@ impl<'lua> rollback_mlua::IntoLua<'lua> for AnimatorPlaybackMode {
         self,
         _lua: &'lua rollback_mlua::Lua,
     ) -> rollback_mlua::Result<rollback_mlua::Value<'lua>> {
-        Ok(rollback_mlua::Value::Number(self as u8 as f64))
+        Ok(rollback_mlua::Value::Integer(self as _))
     }
 }
