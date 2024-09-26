533df34661655c9d2191aed65176dede061fe320
Arthur Cosentino <arthurcose@gmail.com>
2024-09-26 01:05:00 -0400

    Fix fixed card card restriction logic

--- client/src/lua_api/battle_api/built_in/set_fixed_card.lua
+++ client/src/lua_api/battle_api/built_in/set_fixed_card.lua
@@ -70,8 +70,11 @@ local function card_allowed()
     return true
   end
 
-  return ((restriction.code and restriction.code == card_properties.code) or
-    (restriction.package_id and restriction.package_id == card_properties.package_id))
+  if restriction.code then
+    return restriction.code == card_properties.code or card_properties.code == "*"
+  end
+
+  return restriction.package_id == card_properties.package_id
 end
 
 button.use_func = function()
