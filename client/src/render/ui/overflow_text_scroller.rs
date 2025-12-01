2487179b020f8c449ea047d0b75eb84213a88fcb
Arthur Cosentino <arthurcose@gmail.com>
2025-12-01 13:15:37 -0500

    Possibly fix crash when displaying non ascii characters in scrolling text

--- client/src/render/ui/overflow_text_scroller.rs
+++ client/src/render/ui/overflow_text_scroller.rs
@@ -1,6 +1,6 @@
-use std::ops::Range;
-
 use crate::render::FrameTime;
+use std::ops::Range;
+use unicode_segmentation::UnicodeSegmentation;
 
 const MAX_TEXT_WIDTH: usize = 8;
 const FRAMES_PER_CHAR: FrameTime = 20;
@@ -24,10 +24,10 @@ impl OverflowTextScroller {
     }
 
     pub fn text_range(&self, text: &str) -> Range<usize> {
-        let len = text.len();
-        let max_end = len.max(MAX_TEXT_WIDTH) - MAX_TEXT_WIDTH;
+        let grapheme_count = text.graphemes(true).count();
+        let max_end = grapheme_count.max(MAX_TEXT_WIDTH) - MAX_TEXT_WIDTH;
 
-        let index_offset = if max_end == 0 {
+        let grapheme_offset = if max_end == 0 {
             0
         } else {
             let offset =
@@ -43,6 +43,14 @@ impl OverflowTextScroller {
             }
         };
 
-        index_offset..len.min(index_offset + MAX_TEXT_WIDTH)
+        let mut grapheme_iter = text
+            .grapheme_indices(true)
+            .skip(grapheme_offset)
+            .map(|(i, _)| i);
+
+        let start_index = grapheme_iter.clone().next().unwrap_or(text.len());
+        let end_index = grapheme_iter.nth(MAX_TEXT_WIDTH).unwrap_or(text.len());
+
+        start_index..end_index
     }
 }
