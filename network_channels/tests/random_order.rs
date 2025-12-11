f9c335759d0e8a75fa33fdd306f4b5e8c59f57e5
Arthur Cosentino <arthurcose@gmail.com>
2025-12-11 03:30:50 -0500

    Test reliability in network channels test

--- network_channels/tests/random_order.rs
+++ network_channels/tests/random_order.rs
@@ -1,69 +1,106 @@
-// does not test packet reliability
-
 use network_channels::{
     ChannelSender, Config, ConnectionBuilder, Instant, PacketReceiver, PacketSender, Reliability,
     deserialize,
 };
 use rand::{SeedableRng, rngs, seq::SliceRandom};
 
-fn build_connection() -> (ChannelSender<()>, PacketSender<()>, PacketReceiver<()>) {
-    let config = Config {
-        // don't slow down
-        bytes_per_second_slow_factor: 1.0,
+fn create_config() -> Config {
+    Config {
+        // send at max speed
         initial_bytes_per_second: usize::MAX,
         max_bytes_per_second: Some(usize::MAX),
+        // don't speed up
+        bytes_per_second_increase_factor: 1.0,
+        // don't slow down
+        bytes_per_second_slow_factor: 1.0,
+        // retry immediately
+        rtt_resend_factor: 0.0,
         ..Config::default()
-    };
-
-    // A is the sender
-    let mut builder_a = ConnectionBuilder::new(&config);
-    let channel_a = builder_a.sending_channel(());
-    let (a_sender, _) = builder_a.build().split();
-
-    // B is only listening on the other side
-    let mut builder_b = ConnectionBuilder::new(&config);
-    builder_b.receiving_channel(()); // a channel with a matching label must exist to listen
-    let (_, b_receiver) = builder_b.build().split();
-
-    (channel_a, a_sender, b_receiver)
+    }
 }
 
-#[test]
-fn reliable_ordered() {
-    let (channel_a, mut a_sender, mut b_receiver) = build_connection();
+fn create_connection(config: &Config) -> (ChannelSender<()>, PacketSender<()>, PacketReceiver<()>) {
+    let mut builder = ConnectionBuilder::new(config);
+    let channel = builder.bidirectional_channel(());
+    let (sender, receiver) = builder.build().split();
+    (channel, sender, receiver)
+}
+
+fn start_test_environment(
+    send: &mut dyn FnMut(&ChannelSender<()>),
+    receive: &mut dyn FnMut(Vec<Vec<u8>>),
+) {
+    const RUNS: usize = 100;
+    const MESSAGES_PER_RUN: usize = 100;
+    const DROPPED_PER_RUN: usize = 5;
+
+    let config = create_config();
+    // A is the sender
+    let (channel_a, mut sender_a, mut receiver_a) = create_connection(&config);
+    // B is listening and sending acks on the other side
+    let (_, mut sender_b, mut receiver_b) = create_connection(&config);
 
     let mut rng = rngs::SmallRng::from_os_rng();
-    let mut sent_messages = vec![vec![]; 100];
-    let mut next_send_value = 1;
-    let mut last_read_value = 0;
+    let mut sent_messages = vec![vec![]; MESSAGES_PER_RUN];
 
-    for _ in 0..100 {
-        for _ in 0..sent_messages.len() {
-            channel_a.send_serialized(Reliability::ReliableOrdered, next_send_value);
-            next_send_value += 1;
+    for _ in 0..RUNS {
+        for _ in 0..MESSAGES_PER_RUN {
+            send(&channel_a);
         }
 
         // send messages to our messages vec
         let now = Instant::now();
         let mut i = 0;
 
-        a_sender.tick(now, |bytes| {
-            let bucket = &mut sent_messages[i];
+        sender_a.tick(now, |bytes| {
+            let Some(bucket) = sent_messages.get_mut(i) else {
+                return;
+            };
+
             i += 1;
 
             bucket.clear();
             bucket.extend_from_slice(bytes);
         });
 
-        // shuffle messages
-        sent_messages.shuffle(&mut rng);
+        let mut received = 0;
 
-        // read them
-        for message in &sent_messages {
-            let (_channel, read_messages) =
-                b_receiver.receive_packet(now, message).unwrap().unwrap();
+        while received != MESSAGES_PER_RUN {
+            // shuffle messages
+            sent_messages.shuffle(&mut rng);
 
-            for message in read_messages {
+            // read them
+            for message in &sent_messages[..i.saturating_sub(DROPPED_PER_RUN)] {
+                let Some((_channel, read_messages)) =
+                    receiver_b.receive_packet(now, message).unwrap()
+                else {
+                    continue;
+                };
+
+                received += read_messages.len();
+                receive(read_messages);
+            }
+
+            // handle acks
+            sender_b.tick(now, |message| {
+                let _ = receiver_a.receive_packet(now, message);
+            });
+        }
+    }
+}
+
+#[test]
+fn reliable_ordered() {
+    let mut next_send_value = 1;
+    let mut last_read_value = 0;
+
+    start_test_environment(
+        &mut |channel| {
+            channel.send_serialized(Reliability::ReliableOrdered, next_send_value);
+            next_send_value += 1;
+        },
+        &mut |messages| {
+            for message in messages {
                 let read_value = deserialize(&message).unwrap();
 
                 assert_eq!(
@@ -74,8 +111,8 @@ fn reliable_ordered() {
 
                 last_read_value = read_value;
             }
-        }
-    }
+        },
+    );
 
     assert_eq!(
         last_read_value + 1,
@@ -86,43 +123,19 @@ fn reliable_ordered() {
 
 #[test]
 fn reliable() {
-    let (channel_a, mut a_sender, mut b_receiver) = build_connection();
-
-    let mut rng = rngs::SmallRng::from_os_rng();
-    let mut sent_messages = vec![vec![]; 100];
     let mut next_send_value: i32 = 1;
     let mut next_read_value: i32 = 1;
 
     let mut messages_read = 0;
     let mut unresolved_messages = Vec::new();
 
-    for _ in 0..100 {
-        for _ in 0..sent_messages.len() {
-            channel_a.send_serialized(Reliability::Reliable, next_send_value);
+    start_test_environment(
+        &mut |channel| {
+            channel.send_serialized(Reliability::Reliable, next_send_value);
             next_send_value += 1;
-        }
-
-        // send messages to our messages vec
-        let now = Instant::now();
-        let mut i = 0;
-
-        a_sender.tick(now, |bytes| {
-            let bucket = &mut sent_messages[i];
-            i += 1;
-
-            bucket.clear();
-            bucket.extend_from_slice(bytes);
-        });
-
-        // shuffle messages
-        sent_messages.shuffle(&mut rng);
-
-        // read them
-        for message in &sent_messages {
-            let (_channel, read_messages) =
-                b_receiver.receive_packet(now, message).unwrap().unwrap();
-
-            for message in read_messages {
+        },
+        &mut |messages| {
+            for message in messages {
                 let read_value: i32 = deserialize(&message).unwrap();
 
                 unresolved_messages.push(read_value);
@@ -136,10 +149,10 @@ fn reliable() {
                 unresolved_messages.pop();
                 next_read_value += 1;
             }
-        }
-    }
+        },
+    );
 
-    assert!(unresolved_messages.is_empty(), "must receive all messages");
+    assert_eq!(unresolved_messages.len(), 0, "must receive all messages");
 
     assert_eq!(
         messages_read + 1,
