fefb1a80fbb25abe517516e855d05e694750fe67
Arthur Cosentino <arthurcose@gmail.com>
2025-12-12 07:42:31 -0500

    Shift timing for Hit.Root color when it is overwritten

--- client/resources/packages/statuses/root/entry.lua
+++ client/resources/packages/statuses/root/entry.lua
@@ -7,9 +7,12 @@ function status_init(status)
 
   local component = entity:create_component(Lifetime.ActiveBattle)
   local time = 0
+  local apply_color = false
 
   component.on_update_func = function()
-    if time // 2 % 2 == 1 then
+    apply_color = time // 2 % 2 == 1
+
+    if apply_color then
       entity_sprite:set_color_mode(ColorMode.Multiply)
       entity_sprite:set_color(COLOR)
     end
@@ -17,8 +20,33 @@ function status_init(status)
     time = time + 1
   end
 
+  local shift_timing_component = entity:create_component(Lifetime.Scene)
+  shift_timing_component.on_update_func = function()
+    if not apply_color then
+      return
+    end
+
+    apply_color = false
+
+    local color = entity_sprite:color()
+
+    if
+        entity_sprite:color_mode() == ColorMode.Multiply and
+        color.r == COLOR.r and
+        color.g == COLOR.g and
+        color.b == COLOR.b and
+        color.a == COLOR.a
+    then
+      return
+    end
+
+    -- color differs, shift time
+    time = time + 1
+  end
+
   -- clean up
   status.on_delete_func = function()
     component:eject()
+    shift_timing_component:eject()
   end
 end
