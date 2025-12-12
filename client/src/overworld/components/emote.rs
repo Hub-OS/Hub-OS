d23203ac1b7317c2f7e5e794209283b3e4b1cddc
Arthur Cosentino <arthurcose@gmail.com>
2025-12-11 22:22:47 -0500

    Update emotes

--- client/src/overworld/components/emote.rs
+++ client/src/overworld/components/emote.rs
@@ -7,6 +7,7 @@ const MAX_LIFETIME: FrameTime = 60 * 5;
 
 pub struct Emote {
     pub lifetime: FrameTime,
+    pub offset: Vec2,
 }
 
 impl Emote {
@@ -52,8 +53,16 @@ impl Emote {
             return;
         }
 
-        for (_, (emote, attachment, animator)) in
-            entities.query_mut::<(&mut Emote, &ActorAttachment, &mut Animator)>()
+        let initial_offset = Self::resolve_offset(entities, parent_entity).unwrap_or_else(|| {
+            let Ok(parent_animator) = entities.query_one_mut::<&Animator>(parent_entity) else {
+                return Vec2::ZERO;
+            };
+
+            Vec2::new(0.0, -parent_animator.origin().y)
+        });
+
+        for (_, (emote, attachment, animator, offset)) in
+            entities.query_mut::<(&mut Emote, &ActorAttachment, &mut Animator, &mut Vec2)>()
         {
             if attachment.actor_entity != parent_entity {
                 continue;
@@ -63,27 +72,32 @@ impl Emote {
             emote.lifetime = 0;
             animator.set_state(emote_id);
             animator.set_loop_mode(AnimatorLoopMode::Loop);
+
+            // update offset
+            *offset -= emote.offset;
+            emote.offset = animator.point_or_zero("OFFSET");
+            *offset += emote.offset;
+
             return;
         }
 
-        Self::spawn(area, parent_entity, emote_id);
+        Self::spawn(area, parent_entity, emote_id, initial_offset);
     }
 
-    pub fn spawn(area: &mut OverworldArea, parent_entity: hecs::Entity, emote_id: &str) {
-        let entities = &mut area.entities;
-
+    fn spawn(
+        area: &mut OverworldArea,
+        parent_entity: hecs::Entity,
+        emote_id: &str,
+        initial_offset: Vec2,
+    ) {
         let mut animator = area.emote_animator.clone();
         animator.set_state(emote_id);
         animator.set_loop_mode(AnimatorLoopMode::Loop);
 
         let sprite = area.emote_sprite.clone();
-        let offset = Self::resolve_offset(entities, parent_entity).unwrap_or_else(|| {
-            let Ok(parent_animator) = entities.query_one_mut::<&Animator>(parent_entity) else {
-                return Vec2::ZERO;
-            };
+        let frame_offset = animator.point_or_zero("OFFSET");
+        let offset = initial_offset + frame_offset;
 
-            Vec2::new(0.0, -parent_animator.origin().y)
-        });
         let attachment_layer = AttachmentLayer(-1);
         let attachment = ActorAttachment {
             actor_entity: parent_entity,
@@ -91,7 +105,10 @@ impl Emote {
         };
 
         area.entities.spawn((
-            Emote { lifetime: 0 },
+            Emote {
+                lifetime: 0,
+                offset: frame_offset,
+            },
             animator,
             sprite,
             offset,
@@ -121,9 +138,9 @@ impl Emote {
 
         // move to "EMOTE" point
         let mut emote_query = entities.query::<(&Emote, &ActorAttachment, &mut Vec2)>();
-        for (_, (_, attachment, offset)) in emote_query.into_iter() {
+        for (_, (emote, attachment, offset)) in emote_query.into_iter() {
             if let Some(new_offset) = Self::resolve_offset(entities, attachment.actor_entity) {
-                *offset = new_offset;
+                *offset = new_offset + emote.offset;
             }
         }
     }
