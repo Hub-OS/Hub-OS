6b8d2571ef92b8762c7db86c9b603b59b98cb04a
Arthur Cosentino <arthurcose@gmail.com>
2025-12-31 07:22:45 -0500

    Move context menu frame rendering to separate file

--- client/src/render/ui/command_palette.rs
+++ client/src/render/ui/command_palette.rs
@@ -4,11 +4,7 @@ use crate::resources::*;
 use framework::prelude::*;
 
 pub struct CommandPalette<T: Copy + 'static> {
-    label: String,
-    label_9patch: NinePatch,
-    body_9patch: NinePatch,
-    arrow_animator: Animator,
-    animation_time: FrameTime,
+    frame: ContextFrame,
     scroll_tracker: ScrollTracker,
     options: Vec<(String, T)>,
     width: f32,
@@ -18,22 +14,9 @@ pub struct CommandPalette<T: Copy + 'static> {
 impl<T: Copy + 'static> CommandPalette<T> {
     pub fn new(game_io: &GameIO, label_translation_key: &str) -> Self {
         let globals = game_io.resource::<Globals>().unwrap();
-        let assets = &globals.assets;
-
-        // resources
-        let texture = assets.texture(game_io, ResourcePaths::UI_NINE_PATCHES);
-        let animator = Animator::load_new(assets, ResourcePaths::UI_NINE_PATCHES_ANIMATION);
-
-        // styles
-        let label_9patch = build_9patch!(game_io, texture.clone(), &animator, "CONTEXT_LABEL");
-        let body_9patch = build_9patch!(game_io, texture, &animator, "CONTEXT_BODY");
 
         Self {
-            label: globals.translate(label_translation_key),
-            label_9patch,
-            body_9patch,
-            arrow_animator: Animator::load_new(assets, ResourcePaths::MORE_ARROWS_ANIMATION),
-            animation_time: 0,
+            frame: ContextFrame::new(game_io, globals.translate(label_translation_key)),
             scroll_tracker: ScrollTracker::new(game_io, 6).with_wrap(true),
             options: Default::default(),
             width: RESOLUTION_F.x * 0.7,
@@ -121,58 +104,22 @@ impl<T: Copy + 'static> CommandPalette<T> {
         // resolve bounds
         const BODY_TOP_PADDING: f32 = 3.0;
 
-        let mut text_style = TextStyle::new(game_io, FontName::Context);
-        let label_inner_height = text_style.line_height();
-        let label_height = label_inner_height + self.label_9patch.heights();
-        let mut height = label_height + BODY_TOP_PADDING;
-
-        text_style.font = FontName::Thin;
-
-        let line_step_y = text_style.line_height() + text_style.line_spacing;
-        let options_height = line_step_y * self.scroll_tracker.view_range().len() as f32;
-        height += options_height;
-
-        let inner_size = Vec2::new(self.width, height);
-        let top_left = (RESOLUTION_F - inner_size) * 0.5;
-        let body_top_left = top_left + Vec2::new(0.0, label_height);
-
-        self.scroll_tracker.define_cursor(
-            body_top_left + Vec2::new(-8.0, BODY_TOP_PADDING + 1.0),
-            line_step_y,
-        );
-
-        // render label
-        let outer_width = self.width + self.body_9patch.widths() - self.label_9patch.widths();
-        let label_top_left = top_left
-            + Vec2::new(
-                self.label_9patch.left_width() - self.body_9patch.left_width(),
-                self.label_9patch.top_height(),
-            );
-
-        self.label_9patch.draw(
-            sprite_queue,
-            Rect::from_corners(
-                label_top_left,
-                label_top_left + Vec2::new(outer_width, label_inner_height),
-            ),
-        );
-
-        text_style.font = FontName::Context;
-        text_style.bounds.set_position(label_top_left);
-        text_style.draw(game_io, sprite_queue, &self.label);
-
-        // render body
-        self.body_9patch.draw(
-            sprite_queue,
-            Rect::from_corners(body_top_left, top_left + inner_size),
-        );
-
-        // render items
-        text_style.font = FontName::Thin;
+        let mut text_style = TextStyle::new(game_io, FontName::Thin);
         text_style.bounds.width = self.width;
         text_style.bounds.height = text_style.line_height();
 
-        let mut position = body_top_left + Vec2::new(0.0, BODY_TOP_PADDING);
+        let line_step_y = text_style.line_height() + text_style.line_spacing;
+        let options_height = line_step_y * self.scroll_tracker.view_range().len() as f32;
+        let height = BODY_TOP_PADDING + options_height;
+
+        // render frame
+        self.frame.set_inner_size(Vec2::new(self.width, height));
+        self.frame.draw(game_io, sprite_queue);
+
+        // render items
+        let inner_top_left = self.frame.inner_top_left();
+        let mut position = inner_top_left;
+        position.y += BODY_TOP_PADDING;
 
         for i in self.scroll_tracker.view_range() {
             text_style.bounds.set_position(position);
@@ -182,38 +129,15 @@ impl<T: Copy + 'static> CommandPalette<T> {
         }
 
         // render cursor
+        self.scroll_tracker.define_cursor(
+            inner_top_left + Vec2::new(-8.0, BODY_TOP_PADDING + 1.0),
+            line_step_y,
+        );
+
         self.scroll_tracker.draw_cursor(sprite_queue);
 
         // render arrows
-        let globals = game_io.resource::<Globals>().unwrap();
-        let assets = &globals.assets;
-
-        let mut arrow_sprite = assets.new_sprite(game_io, ResourcePaths::MORE_ARROWS);
-
-        let mut draw_state = |sprite: &mut Sprite, state: &str| {
-            self.arrow_animator.set_state(state);
-            self.arrow_animator.set_loop_mode(AnimatorLoopMode::Loop);
-            self.arrow_animator.sync_time(self.animation_time);
-            self.arrow_animator.apply(sprite);
-            sprite_queue.draw_sprite(sprite);
-        };
-
-        let mut arrow_position = body_top_left;
-        arrow_position.x += self.width;
-
-        let view_range = self.scroll_tracker.view_range();
-
-        if view_range.start > 0 {
-            arrow_sprite.set_position(arrow_position);
-            draw_state(&mut arrow_sprite, "ARROW_UP");
-        }
-
-        if view_range.end < self.scroll_tracker.total_items() {
-            arrow_position.y += options_height + BODY_TOP_PADDING + 2.0;
-            arrow_sprite.set_position(arrow_position);
-            draw_state(&mut arrow_sprite, "ARROW_DOWN");
-        }
-
-        self.animation_time += 1;
+        self.frame
+            .draw_scroll_arrows(game_io, sprite_queue, &self.scroll_tracker);
     }
 }
---
+++ client/src/render/ui/context_frame.rs
@@ -0,0 +1,188 @@
+use crate::render::ui::*;
+use crate::render::*;
+use crate::resources::*;
+use framework::prelude::*;
+
+pub struct ContextFrame {
+    label: String,
+    label_size: Vec2,
+    label_9patch: NinePatch,
+    body_9patch: NinePatch,
+    arrow_animator: Animator,
+    animation_time: FrameTime,
+    inner_size: Vec2,
+    outer_size: Vec2,
+    center: Vec2,
+}
+
+impl ContextFrame {
+    pub fn new(game_io: &GameIO, label: String) -> Self {
+        let globals = game_io.resource::<Globals>().unwrap();
+        let assets = &globals.assets;
+
+        // resources
+        let texture = assets.texture(game_io, ResourcePaths::UI_NINE_PATCHES);
+        let animator = Animator::load_new(assets, ResourcePaths::UI_NINE_PATCHES_ANIMATION);
+
+        // styles
+        let label_9patch = build_9patch!(game_io, texture.clone(), &animator, "CONTEXT_LABEL");
+        let body_9patch = build_9patch!(game_io, texture, &animator, "CONTEXT_BODY");
+
+        let label_size = TextStyle::new(game_io, FontName::Context)
+            .measure(&label)
+            .size;
+
+        Self {
+            label,
+            label_size,
+            label_9patch,
+            body_9patch,
+            arrow_animator: Animator::load_new(assets, ResourcePaths::MORE_ARROWS_ANIMATION),
+            animation_time: 0,
+            inner_size: Vec2::ZERO,
+            outer_size: Vec2::ZERO,
+            center: RESOLUTION_F * 0.5,
+        }
+    }
+
+    pub fn with_inner_size(mut self, size: Vec2) -> Self {
+        self.inner_size = size;
+        self.resolve_outer_size();
+        self
+    }
+
+    pub fn with_center(mut self, position: Vec2) -> Self {
+        self.center = position;
+        self
+    }
+
+    pub fn set_inner_size(&mut self, size: Vec2) {
+        self.inner_size = size;
+        self.resolve_outer_size();
+    }
+
+    pub fn set_center(&mut self, position: Vec2) {
+        self.center = position;
+    }
+
+    pub fn inner_top_left(&self) -> Vec2 {
+        let mut top_left = self.center - self.outer_size * 0.5;
+
+        top_left.y += self.label_9patch.heights() + self.label_size.y;
+
+        top_left.x += self.body_9patch.left_width();
+        top_left.y += self.body_9patch.top_height();
+
+        top_left
+    }
+
+    fn resolve_outer_size(&mut self) {
+        // apply inner size
+        self.outer_size = self.inner_size;
+
+        // apply label
+        self.outer_size.x = self.outer_size.x.max(self.label_size.x);
+        self.outer_size.y += self.label_size.y;
+
+        // apply frame
+        self.outer_size.y += self.label_9patch.heights() + self.body_9patch.heights();
+        self.outer_size.x += self.label_9patch.widths().max(self.body_9patch.widths());
+    }
+
+    pub fn draw(&mut self, game_io: &GameIO, sprite_queue: &mut SpriteColorQueue) {
+        let outer_top_left = self.center - self.outer_size * 0.5;
+
+        // render label
+        let label_border_top = self.label_9patch.top_height();
+        let label_border_bottom = self.label_9patch.bottom_height();
+        let label_border_heights = label_border_top + label_border_bottom;
+
+        let label_top_left = outer_top_left
+            + Vec2::new(
+                self.label_9patch.left_width(),
+                self.label_9patch.top_height(),
+            );
+
+        let mut text_style = TextStyle::new(game_io, FontName::Context);
+        text_style.bounds.set_position(label_top_left);
+
+        let label_size = Vec2::new(
+            self.outer_size.x - self.label_9patch.widths(),
+            text_style.line_height(),
+        );
+
+        self.label_9patch.draw(
+            sprite_queue,
+            Rect::from_corners(label_top_left, label_top_left + label_size),
+        );
+
+        text_style.font = FontName::Context;
+        text_style.draw(game_io, sprite_queue, &self.label);
+
+        // render body
+        let mut body_top_left = outer_top_left;
+        body_top_left.x += self.body_9patch.left_width();
+        body_top_left.y +=
+            label_border_heights + text_style.line_height() + self.body_9patch.top_height();
+
+        let mut body_bottom_right = self.center + self.outer_size * 0.5;
+        body_bottom_right.x -= self.body_9patch.right_width();
+        body_bottom_right.y -= self.body_9patch.bottom_height();
+
+        self.body_9patch.draw(
+            sprite_queue,
+            Rect::from_corners(body_top_left, body_bottom_right),
+        );
+    }
+
+    pub fn draw_scroll_arrows(
+        &mut self,
+        game_io: &GameIO,
+        sprite_queue: &mut SpriteColorQueue,
+        scroll_tracker: &ScrollTracker,
+    ) {
+        let globals = game_io.resource::<Globals>().unwrap();
+        let assets = &globals.assets;
+
+        let mut arrow_sprite = assets.new_sprite(game_io, ResourcePaths::MORE_ARROWS);
+
+        let mut draw_state = |sprite: &mut Sprite, state: &str| {
+            self.arrow_animator.set_state(state);
+            self.arrow_animator.set_loop_mode(AnimatorLoopMode::Loop);
+            self.arrow_animator.sync_time(self.animation_time);
+            self.arrow_animator.apply(sprite);
+            sprite_queue.draw_sprite(sprite);
+        };
+
+        let view_range = scroll_tracker.view_range();
+
+        if view_range.start > 0 {
+            // snap to corner
+            let mut arrow_position = self.center;
+            arrow_position.x += self.outer_size.x * 0.5;
+            arrow_position.y -= self.outer_size.y * 0.5;
+
+            // account for borders
+            arrow_position.x -= self.body_9patch.right_width();
+            arrow_position.y += self.label_9patch.heights()
+                + TextStyle::new(game_io, FontName::Context).line_height();
+
+            arrow_sprite.set_position(arrow_position);
+            draw_state(&mut arrow_sprite, "ARROW_UP");
+        }
+
+        if view_range.end < scroll_tracker.total_items() {
+            // snap to corner
+            let mut arrow_position = self.center + self.outer_size * 0.5;
+
+            // account for borders
+            arrow_position.x -= self.body_9patch.right_width();
+            arrow_position.y -= self.body_9patch.bottom_height() - 2.0;
+
+            arrow_sprite.set_position(arrow_position);
+            draw_state(&mut arrow_sprite, "ARROW_DOWN");
+        }
+
+        self.animation_time += 1;
+    }
+}
